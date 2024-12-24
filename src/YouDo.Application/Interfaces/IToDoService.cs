0bfa64355b3fed3712cc50eb27ffd3b984f8268e
tmarcorf <tmarco783@gmail.com>
2024-12-23 22:00:39 -0300

    feat: definindo interface do serviço

--- src/YouDo.Application/Interfaces/IToDoService.cs
+++ src/YouDo.Application/Interfaces/IToDoService.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YouDo.Application.DTOs;
+using YouDo.Core.Entities;
 
 namespace YouDo.Application.Interfaces
 {
     public interface IToDoService
     {
+        Task<IEnumerable<ToDoDTO>> GetAllFromUserAsync(Guid userId);
 
+        Task<IEnumerable<ToDoDTO>> GetAllFromUserWithSpecifiedCreationDateAsync(Guid userId, DateTime creationDate);
+
+        Task<ToDoDTO> GetByIdAsync(Guid id);
+
+        Task<ToDoDTO> CreateAsync(ToDoDTO toDo);
+
+        Task<ToDoDTO> UpdateAsync(ToDoDTO toDo);
+
+        Task<bool> DeleteAsync(Guid id);
     }
 }
