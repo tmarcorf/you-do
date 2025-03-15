a968fe99215880687786bdd640df53aeb98a7993
tmarcorf <tmarco783@gmail.com>
2025-03-14 23:14:35 -0300

    chore: removingo data annotations

--- src/YouDo.Application/DTOs/Authenticate/LoginUserDTO.cs
+++ src/YouDo.Application/DTOs/Authenticate/LoginUserDTO.cs
@@ -4,12 +4,8 @@ namespace YouDo.Application.DTOs.Authenticate
 {
     public class LoginUserDTO
     {
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid format email")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
-        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
