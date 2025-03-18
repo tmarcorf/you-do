25d3f6099c4bd408c9b92de65b4c68303ee7e41d
tmarcorf <tmarco783@gmail.com>
2025-03-17 22:18:35 -0300

    chore: changing the property name Error to Message on ApiResponse

--- src/YouDo.API/Responses/ApiResponse.cs
+++ src/YouDo.API/Responses/ApiResponse.cs
@@ -13,18 +13,18 @@ namespace YouDo.API.Responses
         [JsonPropertyName("is_success")]
         public bool IsSuccess { get; private set; }
 
-        [JsonPropertyName("error")]
-        public string Error { get; private set; }
+        [JsonPropertyName("message")]
+        public string Message { get; private set; }
 
         [JsonPropertyName("timestamp")]
         public DateTime Timestamp => DateTime.UtcNow;
 
-        public ApiResponse(string code, T data, bool isSuccess, string error = null)
+        public ApiResponse(string code, T data, bool isSuccess, string message = null)
         {
             Code = code;
             Data = data;
             IsSuccess = isSuccess;
-            Error = error;
+            Message = message;
         }
 
         public static ApiResponse<T> Success(T data, string code = "200")
@@ -32,9 +32,9 @@ namespace YouDo.API.Responses
             return new ApiResponse<T>(code, data, true);
         }
 
-        public static ApiResponse<T> Failure(string error, string code = "400")
+        public static ApiResponse<T> Failure(string message, string code = "400")
         {
-            return new ApiResponse<T>(code, default, false, error);
+            return new ApiResponse<T>(code, default, false, message);
         }
     }
 }
