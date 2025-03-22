8edae5730590bb881c9738074574f7aadb84d61e
tmarcorf <tmarco783@gmail.com>
2025-03-22 16:41:30 -0300

    feat: adding the frontend Angular project
    I prefer to working on frontend in a separate branch, just to keep the things clear and organized

---
+++ src/frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'You-Do App';
+}
