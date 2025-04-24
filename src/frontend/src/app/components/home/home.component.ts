8bc724abe696adaf812981293c765414cc7e0ce5
tmarcorf <tmarco783@gmail.com>
2025-04-24 15:39:57 -0300

    feat: adding LoginService

--- src/frontend/src/app/components/home/home.component.ts
+++ src/frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, inject, Input, Output } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import { LoginService } from '../../services/LoginService';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +9,20 @@ import { Component, inject, Input, Output } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  
+  private _snackBar = inject(MatSnackBar);
+
+  constructor(){
+    
+  }
+
+  showSnackBar() {
+    var config = new MatSnackBarConfig();
+
+    config.duration = 1000;
+    config.announcementMessage = "annoucement";
+
+
+    var retorno = this._snackBar.open('teste', "Ok", config);
+    console.log(retorno);
+  }
 }
